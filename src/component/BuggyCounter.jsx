057f6b20017fa1f932eea7fcea6c937e5c0af058
Justin Rahimi <9245522+Tellisense@users.noreply.github.com>
2021-05-02 12:23:42 -0700

    initial commit

---
+++ src/component/BuggyCounter.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+const BuggyCounter = () => {
+
+  const [counter, setCounter] = useState(0)
+
+
+  const handleClick = () => {
+    setCounter(counter + 1)
+  }
+
+  if (counter === 5) {
+    // Simulate a JS error
+    throw new Error("I crashed the buggy app!");
+  }
+
+
+  return <h1 onClick={handleClick}>{counter}</h1>;
+}
+
+export default BuggyCounter
